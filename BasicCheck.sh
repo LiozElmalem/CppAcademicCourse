b9dea9951536ad245c4468f431c28bd9558d14e1
Elmalem <44621326+Elmalem@users.noreply.github.com>
2019-03-12 15:10:05 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -7,26 +7,22 @@ arguments=${@:3}
 #  we search the makefile file in the current path
 #  if the makefile doesn't exist the value be different from 0 and the script will exit. 
 #  else, the script will check compilation , Memory leaks and thread race.
-#  the script returns binary number that represent which of the tests work and who fail. 
-find Makefile &> /dev/null
-if [[ $? -gt 0 ]]; then
-	exit 0;
-fi
-clang++-5.0 $program.cpp &> /dev/null
+#  the script returns binary number that represent which of the tests work and who fail.
+make &> /dev/null
 if [[ $? -gt 0 ]]; then
 	firstANS=1
 	exit 1
  else
 	 firstANS=0
 	 # valgrind memory leak check
-	 valgrind --tool=memcheck --leak-check=full --error-exitcode=3 -q ./a.out $arguments &> /dev/null
+	 valgrind --tool=memcheck --leak-check=full --error-exitcode=3 -q ./$program $arguments &> /dev/null
 	 if [[ $? -gt 0 ]] ; then
 		 secondANS=1
 	 	 else
 		 secondANS=0
 	 fi
 	 # valgrind-helgrind thread race check
-	 valgrind --tool=helgrind -q ./a.out $arguments &> /dev/null
+	 valgrind --tool=helgrind -q ./$program $arguments &> /dev/null
 	 if [[ $? -gt 0 ]]; then
 		 thirdANS=1
 	 else
@@ -38,18 +34,20 @@ if [[ $? -gt 0 ]]; then
  ANS=$firstANS$secondANS$thirdANS
  if [ $ANS == '000' ]; then
 	 echo -e "Compilation   Memory leaks   Thread race\n    PASS         PASS           PASS"
-	 exit 7;
+	 exit 0;
 fi
 if [ $ANS == '010' ]; then
 	echo -e "Compilation   Memory leaks   Thread race\n    PASS         FAIL           PASS"
-	exit 7;
+	exit 2;
 fi
 if [ $ANS == '001' ]; then
-	echo -e "Compilation   Memory leaks   Thread race\n    PASS         PASS           FAIL"	exit 7;
+	echo -e "Compilation   Memory leaks   Thread race\n    PASS         PASS           FAIL"
+	exit 1;
 fi
 
 if [ $ANS == '011' ]; then
-	echo -e "Compilation   Memory leaks   Thread race\n    PASS         FAIL           FAIL"	exit 7;
+	echo -e "Compilation   Memory leaks   Thread race\n    PASS         FAIL           FAIL"
+	exit 3;
  else
  echo -e "wrong program  - 111"
  exit 7;
